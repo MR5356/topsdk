64164d25e6f54746408b4d04a9fe0172c1f9055c
Rui Ma <535657150@qq.com>
2023-10-14 20:23:27 +0800

    update mod name

--- ability369/Ability369.go
+++ ability369/Ability369.go
@@ -1,57 +1,58 @@
 package ability369
 
 import (
-    "log"
-    "errors"
-    "topsdk"
-    "topsdk/ability369/request"
-    "topsdk/ability369/response"
-    "topsdk/util"
+	"errors"
+	"github.com/MR5356/topsdk"
+	"github.com/MR5356/topsdk/ability369/request"
+	"github.com/MR5356/topsdk/ability369/response"
+	"github.com/MR5356/topsdk/util"
+	"log"
 )
 
 type Ability369 struct {
-    Client *topsdk.TopClient
+	Client *topsdk.TopClient
 }
 
-func NewAbility369(client *topsdk.TopClient) *Ability369{
-    return &Ability369{client}
+func NewAbility369(client *topsdk.TopClient) *Ability369 {
+	return &Ability369{client}
 }
 
 /*
-    淘宝客-推广者-物料精选
+淘宝客-推广者-物料精选
 */
-func (ability *Ability369) TaobaoTbkDgOptimusMaterial(req *request.TaobaoTbkDgOptimusMaterialRequest) (*response.TaobaoTbkDgOptimusMaterialResponse,error) {
-    if(ability.Client == nil) {
-        return nil,errors.New("Ability369 topClient is nil")
-    }
-    var jsonStr,err = ability.Client.Execute("taobao.tbk.dg.optimus.material",req.ToMap(),req.ToFileMap())
-    var respStruct = response.TaobaoTbkDgOptimusMaterialResponse{}
-    if(err != nil){
-        log.Println("taobaoTbkDgOptimusMaterial error",err)
-        return &respStruct,err
-    }
-    err = util.HandleJsonResponse(jsonStr, &respStruct)
-    if(respStruct.Body == "" || len(respStruct.Body) == 0) {
-        respStruct.Body = jsonStr
-    }
-    return &respStruct,err
+func (ability *Ability369) TaobaoTbkDgOptimusMaterial(req *request.TaobaoTbkDgOptimusMaterialRequest) (*response.TaobaoTbkDgOptimusMaterialResponse, error) {
+	if ability.Client == nil {
+		return nil, errors.New("Ability369 topClient is nil")
+	}
+	var jsonStr, err = ability.Client.Execute("taobao.tbk.dg.optimus.material", req.ToMap(), req.ToFileMap())
+	var respStruct = response.TaobaoTbkDgOptimusMaterialResponse{}
+	if err != nil {
+		log.Println("taobaoTbkDgOptimusMaterial error", err)
+		return &respStruct, err
+	}
+	err = util.HandleJsonResponse(jsonStr, &respStruct)
+	if respStruct.Body == "" || len(respStruct.Body) == 0 {
+		respStruct.Body = jsonStr
+	}
+	return &respStruct, err
 }
+
 /*
-    淘宝客-推广者-权益物料精选
+淘宝客-推广者-权益物料精选
 */
-func (ability *Ability369) TaobaoTbkDgOptimusPromotion(req *request.TaobaoTbkDgOptimusPromotionRequest) (*response.TaobaoTbkDgOptimusPromotionResponse,error) {
-    if(ability.Client == nil) {
-        return nil,errors.New("Ability369 topClient is nil")
-    }
-    var jsonStr,err = ability.Client.Execute("taobao.tbk.dg.optimus.promotion",req.ToMap(),req.ToFileMap())
-    var respStruct = response.TaobaoTbkDgOptimusPromotionResponse{}
-    if(err != nil){
-        log.Println("taobaoTbkDgOptimusPromotion error",err)
-        return &respStruct,err
-    }
-    err = util.HandleJsonResponse(jsonStr, &respStruct)
-    if(respStruct.Body == "" || len(respStruct.Body) == 0) {
-        respStruct.Body = jsonStr
-    }
-    return &respStruct,err
+func (ability *Ability369) TaobaoTbkDgOptimusPromotion(req *request.TaobaoTbkDgOptimusPromotionRequest) (*response.TaobaoTbkDgOptimusPromotionResponse, error) {
+	if ability.Client == nil {
+		return nil, errors.New("Ability369 topClient is nil")
+	}
+	var jsonStr, err = ability.Client.Execute("taobao.tbk.dg.optimus.promotion", req.ToMap(), req.ToFileMap())
+	var respStruct = response.TaobaoTbkDgOptimusPromotionResponse{}
+	if err != nil {
+		log.Println("taobaoTbkDgOptimusPromotion error", err)
+		return &respStruct, err
+	}
+	err = util.HandleJsonResponse(jsonStr, &respStruct)
+	if respStruct.Body == "" || len(respStruct.Body) == 0 {
+		respStruct.Body = jsonStr
+	}
+	return &respStruct, err
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module topsdk
+module github.com/MR5356/topsdk
 
 go 1.16
